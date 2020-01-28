2366ffd65118ee7e83830862000689ed11afbd86
Doug Crozier <gituser@dougcrozier.com>
2020-01-27 21:54:54 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Spinner from "./windup-spinner.svelte"
+</script>
+
+<main>
+  <div class="placeholder">
+    <Spinner></Spinner>
+  </div>
+</main>
+
+<style>
+  .placeholder {
+    height: 100px;
+    width: 100px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+  }
+</style>
---
+++ src/windup-spinner.svelte
@@ -0,0 +1,43 @@
+<div class="box"></div>
+
+<style>
+  @keyframes shrink1 {
+    0% {transform: matrix(1,0,0,1,0,0);}
+    90% {transform: matrix(0.000001,0,0,0.0000001,0,0);}
+    100% {transform: matrix(1,0,0,1,0,0);}
+  }
+
+  @keyframes shrink2 {
+    0% {transform: matrix(1,0,0,1,0,0);}
+    45% {transform: matrix(1,0,0,0.5,0,0);}
+    90% {transform: matrix(0.000001,0,0,0.0000001,0,0);}
+    100% {transform: matrix(1,0,0,1,0,0);}
+  }
+
+  @keyframes aniMatrixWindUp {
+    from, to {transform: matrix(1,0,0,1,0,0);}
+    25% {transform: matrix(0,1,-1,0,0,0);}
+    50% {transform: matrix(-1,0,0,-1,0,0);}
+    75% {transform: matrix(0,-1,1,0,0,0);}
+  } /* Use with default (ease) timing.  5s duration works well */
+
+  @keyframes aniMatrixSkew1 {
+    from, to {transform: matrix(1,0,0,1,0,0);}
+    90% {transform: matrix(1,1,2,1,0,0);}
+  }
+
+  @keyframes aniRotate {
+    from {transform: rotate(0deg);}
+    to {transform: rotate(360deg);}
+  }
+
+  .box {
+    height: 100px;
+    width: 100px;
+    padding: 10px;
+    background-color: #697889;
+    animation-name: aniMatrixWindUp;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+  }
+</style>
